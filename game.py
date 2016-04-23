282ff7c1c1778becfad998a44d4b91cf0b6a0d2c
Joe Reich <jreich@covermymeds.com>
2016-04-23 18:55:06 -0400

    add algorithm for updating the board after a move

--- game.py
+++ game.py
@@ -21,11 +21,32 @@ class Game(object):
             print ' '.join(row)
         print ''
 
-    def move_in_bounds(self, coordinates):
-        for coord in coordinates:
+    def update_with_move(self, coordinates, symbol):
+        row_index = coordinates[0]
+        col_index = coordinates[1]
+        self.state[row_index][col_index] = symbol
+
+        for row_delta in [-1, 0, 1]:
+            for col_delta in [-1, 0, 1]:
+                self.adjust_pieces(row_index, col_index, row_delta, col_delta, symbol)
+
+    def adjust_pieces(self, row, col, row_delta, col_delta, symbol):
+        next_row = row + row_delta
+        next_col = col + col_delta
+
+        if self.state[next_row][next_col] == '.': return False
+        if self.move_out_of_bounds(next_row, next_col): return False
+        if self.state[next_row][next_col] == symbol: return True
+
+        if self.adjust_pieces(next_row, next_col, row_delta, col_delta, symbol):
+            self.state[next_row][next_col] = symbol
+            return True
+
+    def move_out_of_bounds(self, row, col):
+        for coord in [row, col]:
             if coord < 0 or coord >= self.board_size:
-                return False
-        return True
+                return True
+        return False
 
     def move_free_spot(self, coordinates):
         row = coordinates[0]
--- test_game.py
+++ test_game.py
@@ -16,15 +16,30 @@ class GameTest(unittest.TestCase):
 
         self.assertTrue('Number of rows must be even' in context.exception)
 
-    def test_move_in_bounds(self):
+    def test_update_with_move(self):
+        game = Game(4)
+        initial_state =  [['.','.','.','.'],
+                          ['.','X','O','.'],
+                          ['.','O','X','.'],
+                          ['O','X','O','.']]
+        game.state = initial_state
+        coordinates = [0,2]
+        expected_state = [['.','.','X','.'],
+                          ['.','X','X','.'],
+                          ['.','O','X','.'],
+                          ['O','X','O','.']]
+        game.update_with_move(coordinates, 'X')
+        self.assertEqual(game.state, expected_state)
+
+    def test_move_out_of_bounds(self):
         game = Game(4)
         in_bounds_moves = [[2,2], [0,3], [3,0]]
         for move in in_bounds_moves:
-            self.assertTrue(game.move_in_bounds(move))
+            self.assertFalse(game.move_out_of_bounds(move[0], move[1]))
 
         out_of_bounds_moves = [[2,4], [4,2], [-1,1], [1,-1]]
         for move in out_of_bounds_moves:
-            self.assertFalse(game.move_in_bounds(move))
+            self.assertTrue(game.move_out_of_bounds(move[0], move[1]))
 
     def test_move_free_spot(self):
         game = Game(4)
